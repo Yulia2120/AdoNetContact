b5088e1c48f2e23c15cf762e66fb7a8819903122
Yulia2120 <ket-dz@rambler.ru>
2022-03-20 20:57:16 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetContact
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection SqlConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactDB"].ConnectionString);
+            SqlConnection.Open();
+            if(SqlConnection != null)
+            {
+                MessageBox.Show("Подключение установлено!");
+            }
+        }
+    }
+}
