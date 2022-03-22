1ee2c1ffb177fdf5ca03100eb97e672bec3db420
Yulia2120 <ket-dz@rambler.ru>
2022-03-22 22:51:42 +0200

    New create table plus delete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,23 @@
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.butInsert = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.butInsertAddress = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl
@@ -47,23 +63,143 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.butInsert);
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(767, 424);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "InsertUsers";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.butInsertAddress);
+            this.tabPage2.Controls.Add(this.textBox12);
+            this.tabPage2.Controls.Add(this.textBox11);
+            this.tabPage2.Controls.Add(this.textBox10);
+            this.tabPage2.Controls.Add(this.textBox9);
+            this.tabPage2.Controls.Add(this.textBox8);
+            this.tabPage2.Controls.Add(this.textBox7);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(767, 424);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "InsertAddress";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(224, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(332, 35);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(440, 35);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(548, 35);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 0;
+            // 
+            // butInsert
+            // 
+            this.butInsert.BackColor = System.Drawing.Color.MistyRose;
+            this.butInsert.Location = new System.Drawing.Point(665, 32);
+            this.butInsert.Name = "butInsert";
+            this.butInsert.Size = new System.Drawing.Size(94, 23);
+            this.butInsert.TabIndex = 1;
+            this.butInsert.Text = "INSERT";
+            this.butInsert.UseVisualStyleBackColor = false;
+            this.butInsert.Click += new System.EventHandler(this.butInsert_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(20, 34);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(126, 34);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 0;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(232, 34);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 0;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(338, 34);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 0;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(444, 34);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 0;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(550, 34);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 0;
+            // 
+            // butInsertAddress
+            // 
+            this.butInsertAddress.BackColor = System.Drawing.Color.MistyRose;
+            this.butInsertAddress.Location = new System.Drawing.Point(665, 32);
+            this.butInsertAddress.Name = "butInsertAddress";
+            this.butInsertAddress.Size = new System.Drawing.Size(94, 23);
+            this.butInsertAddress.TabIndex = 1;
+            this.butInsertAddress.Text = "INSERT";
+            this.butInsertAddress.UseVisualStyleBackColor = false;
+            this.butInsertAddress.Click += new System.EventHandler(this.butInsertAddress_Click);
             // 
             // Form1
             // 
@@ -75,6 +211,10 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -84,6 +224,20 @@
         private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button butInsert;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button butInsertAddress;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,10 +24,40 @@ namespace AdoNetContact
         {
             SqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ContactDB"].ConnectionString);
             SqlConnection.Open();
-            if(SqlConnection != null)
-            {
-                MessageBox.Show("Подключение установлено!");
-            }
+            //if(SqlConnection != null)
+            //{
+            //    MessageBox.Show("Подключение установлено!");
+            //}
+        }
+
+        private void butInsert_Click(object sender, EventArgs e)
+        {
+            //SqlCommand command = new SqlCommand($"DELETE FROM [Users] WHERE Id = 11", SqlConnection); //(FirstName, Surname, LastName, Birthday, Phone, DataAdded) VALUES (@FirstName, @Surname, @LastName, @Birthday, @Phone, @DataAdded)", SqlConnection);
+     
+            //command.Parameters.AddWithValue("FirstName", textBox1.Text);
+            //command.Parameters.AddWithValue("Surname", textBox2.Text);
+            //command.Parameters.AddWithValue("LastName", textBox3.Text);
+            //command.Parameters.AddWithValue("Birthday", textBox4.Text);
+            //command.Parameters.AddWithValue("Phone", textBox5.Text);
+            //command.Parameters.AddWithValue("DataAdded", textBox6.Text);
+           // command.ExecuteNonQuery();
+           // MessageBox.Show("Inserted");
+
+        }
+       
+        private void butInsertAddress_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"INSERT INTO [Address](UserId, Country, City, Street, House, Apartment ) VALUES (@UserId, @Country, @City, @Street, @House, @Apartment )", SqlConnection);
+
+            command.Parameters.AddWithValue("UserId", textBox1.Text);
+            command.Parameters.AddWithValue("Country", textBox2.Text);
+            command.Parameters.AddWithValue("City", textBox3.Text);
+            command.Parameters.AddWithValue("Street", textBox4.Text);
+            command.Parameters.AddWithValue("House", textBox5.Text);
+            command.Parameters.AddWithValue("Apartment ", textBox6.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Inserted");
+
         }
     }
 }
