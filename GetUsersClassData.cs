cadfc53d13e28a7bfb92c26778035c91fabdaedb
Yulia2120 <ket-dz@rambler.ru>
2022-04-04 18:08:35 +0300

    class GetUsersClassData

---
+++ GetUsersClassData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace AdoNetContact
+{
+    public static class GetUsersClassData
+    {
+        //public static IList<Users> GetUsers()
+        //{
+        //    using (IDbConnection db = new SqlConnection(SqlConnectionClass.ConnectionString))
+        //    {
+        //        if (db.State == ConnectionState.Closed)
+        //            db.Open();
+        //        return db.Query<Users>("".ToList();
+        //    }
+        //}
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetContact
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string LastName { get; set; }
+        public string Birthday { get; set; }
+        public string Phone { get; set; }
+        public string DataAdded { get; set; }
+
+        public Users() { }
+
+    }
+}
